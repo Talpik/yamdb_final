9aeca08741ff7c56bb49e096b61c02b1db600035
Talpa <talpa1989@gmail.com>
2021-04-24 03:25:57 +0300

    start.sh3

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo docker exec -it yamdb_final_web_1 python3 manage.py migrate --noinput
-sudo docker exec -it yamdb_final_web_1 python3 manage.py loaddata fixtures.json
-sudo docker exec -it yamdb_final_web_1 python3 manage.py collectstatic --noinput
+sudo docker exec -T yamdb_final_web_1 python3 manage.py migrate --noinput
+sudo docker exec -T yamdb_final_web_1 python3 manage.py loaddata fixtures.json
+sudo docker exec -T yamdb_final_web_1 python3 manage.py collectstatic --noinput
